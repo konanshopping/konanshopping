f4cf0dd28a624f403c3130467abc227ca8aa0395
konanshopping <konanshoppingcameroun@gmail.com>
2026-06-06 11:19:50 +0200

    Fix boutique mobile layout

--- src/pages/Boutique.jsx
+++ src/pages/Boutique.jsx
@@ -2680,7 +2680,7 @@ useEffect(() => {
   window.innerWidth < 768
     ? "repeat(2, 1fr)"
     : "repeat(4, 1fr)",
-            gap: "25px",
+            gap: window.innerWidth < 768 ? "15px" : "25px",
           }}
         >
 
