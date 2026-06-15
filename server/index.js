94634860dca8de179cf50f95a50187f343a70133
konanshopping <konanshoppingcameroun@gmail.com>
2026-06-15 16:16:57 +0200

    gmail fix

--- server/index.js
+++ server/index.js
@@ -38,9 +38,7 @@ const Coupon =
   require("./models/Visitor");
 
   const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
