983e12830187010a6b3e8ef8285efe9047f68b3a
konanshopping <konanshoppingcameroun@gmail.com>
2026-06-16 16:28:32 +0200

    UI compte utilisateur premium

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,14 +1,15 @@
 import {
   useState,
+  useEffect,
   useRef
 } from "react";
-
-import { useState } from "react";
 
 import {
   Link,
   useNavigate,
 } from "react-router-dom";
+
+import axios from "axios";
 
 import {
   FaHeart,
